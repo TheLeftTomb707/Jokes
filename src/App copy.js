633377844406c3a887791c02e9a3941ab7425430
TheLeftTomb707 <50126797+TheLeftTomb707@users.noreply.github.com>
2022-11-01 14:31:09 -0700

    Add files via upload

--- src/App copy.js
+++ src/App copy.js
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from "react";
-import "./App.css"
 
 const App = () => {
-    const [quote, setQuote] = useState("");
+    const [country, setCountry] = useState("");
 
     useEffect(() => {
         async function Data() {
-        var response = await fetch(`https://api.chucknorris.io/jokes/random`);
+        var response = await fetch(`https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`);
         const json = await response.json();
         var list = [];
         list.push( <>
-            <h1>{json.value}</h1>
+            <h1>{json.quotes[Math.floor(Math.random()*102)].quote}</h1>
         </>
         )
-        setQuote(list);
+        setCountry(list);
         }
         Data()
     }, []);
 
     return (
         <div id="content">
-            {quote}
-            <a href="/"><button id="but">Get New Quote</button></a>
+            {country}
         </div>
     );
 };
